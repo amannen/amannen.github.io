65baf2bbe60a0ae79e3dbc5b4f03d5f72607c9d1
amannen <alyssa.mannen@gmail.com>
2018-11-12 20:18:56 -0600

    Commit changes to add side bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-alert("This is an alert I created in index.js!");
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    }
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    }
